98ca714e3d679ddcc6a1daf7e4d998be801f3887
Dylan <kite.d92@gmail.com>
2018-11-13 20:08:34 -0800

    added crud functionality via knex, added .gitignore

--- backend/controllers/movies.js
+++ backend/controllers/movies.js
@@ -1,8 +1,62 @@
 const model = require('../models/movies')
 
-function getAllMovies(req, res){
-    const movies = model.getAllMovies()
-    res.send({data: movies})
+function getAllMovies(req, res, next){
+    return model.getAllMovies()
+        .then(result => {
+            res.send({ data: result })
+        })
+        .catch(error => {
+            next(error)
+        })
 }
 
-module.exports = {getAllMovies}
+function getOneMovie(req, res, next) {
+    const id = req.params.id
+    return model.getOneMovie(id)
+        .then(result => {
+            if(!result.length) return next({status:404, message:'The movie you\'re looking for isn\'t here'})
+            res.send({data: result})
+        })
+        .catch(error => {
+            next(error)
+        })
+}
+
+function createMovie(req, res, next){
+    const {director, poster_url, rating, title, year} = req.body
+    if (!director || !poster_url || !rating || !title || !year) return next({status: 400, message: 'You must provide all fields'})
+    return model.createMovie(req.body)
+        .then(result => {
+            res.send({data:result})
+        })
+        .catch(error => {
+            next(error)
+        })
+}
+
+function editMovie(req, res, next){
+    const id = req.params.id
+    return model.editMovie(id, req.body)
+        .then(result => {
+            if (!result) return next({ status: 404, message: 'The movie you\'re looking for isn\'t here' })
+            res.send({data:result})
+        })
+        .catch(error => {
+            next(error)
+        })
+}
+
+function deleteMovie(req, res, next){
+    const id = req.params.id
+    return model.deleteMovie(id)
+        .then(result => {
+            if (!result) return next({ status: 404, message: 'The movie you\'re looking for isn\'t here' })
+            res.send({data:result}) 
+        })
+        .catch(error => {
+            next(error)
+        })
+}
+
+
+module.exports = {getAllMovies, getOneMovie, createMovie, editMovie, deleteMovie}
--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -3,5 +3,8 @@ const router = express.Router()
 const ctrl = require('../controllers/movies')
 
 router.get('/', ctrl.getAllMovies)
-
+router.get('/:id', ctrl.getOneMovie)
+router.post('/', ctrl.createMovie)
+router.put('/:id', ctrl.editMovie)
+router.delete('/:id', ctrl.deleteMovie)
 module.exports = router
